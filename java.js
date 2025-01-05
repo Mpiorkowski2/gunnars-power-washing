d7fd0eef9dc4e9b02be32fcee51e966f40915e8d
Mpiorkowski2 <piorkowskimartin83@gmail.com>
2025-01-05 17:47:49 -0600

    almost done completley

--- java.js
+++ java.js
@@ -1,16 +1,6 @@
-/* EMAIL SIGN UP*/
-/* EMAIL SIGN UP */
-function showThankYouMessage(event) {
-    event.preventDefault();  // Prevents the form from actually submitting
-  
-    // Hide the form
-    document.getElementById('subscribe-form').style.display = 'none';
-  
-    // Show the "Thank You" message
-    document.getElementById('thank-you-message').style.display = 'block';
-  
-    // The form will automatically be submitted to the Google Sheets backend
-}
+
+
+/* CAROUSEL CODE */
   
 document.addEventListener("DOMContentLoaded", () => {
   const holder = document.querySelector(".carousel_items");  // Updated to use .carousel_items
@@ -55,3 +45,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   infiniteScroll();
 });
+
+
+
